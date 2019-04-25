0ec2de213ab3355711f56b6ef7f4ef0f56954cd4
Unknown <seaninho10@gmail.com>
2019-04-25 21:50:50 +0300

    shapeFilter added

--- HW1/src/shapes.erl
+++ HW1/src/shapes.erl
@@ -13,7 +13,9 @@
 -export([shapesArea/1]).
 -export([squaresArea/1]).
 -export([trianglesArea/1]).
+-export([shapesFilter/1]).
 
+%% This function gets a shapes data-structure {shapes, [list of shapes]} and returns the sum of all areas
 shapesArea({shapes,[]}) -> 0;
 shapesArea({shapes,[H|T]}) ->
   case getShape(H) of
@@ -22,14 +24,15 @@ shapesArea({shapes,[H|T]}) ->
     ellipse -> calcAreaEllipse(getShapeDim(H)) + shapesArea({shapes, T})
   end.
 
-
+%% This function gets a shapes data-structure {shapes, [list of shapes]} and returns the sum of all triangle areas
 trianglesArea({shapes,[]}) -> 0;
 trianglesArea({shapes,[H|T]}) ->
    case getShape(H) == triangle of
       true -> calcAreaTriangle(getShapeDim(H)) + trianglesArea({shapes, T});
       false -> 0 + trianglesArea({shapes,T})
    end.
-   
+
+%% This function gets a shapes data-structure {shapes, [list of shapes]} and returns the sum of all square areas
 squaresArea({shapes,[]}) -> 0;
 squaresArea({shapes, [H|T]})  ->
    case getShape(H) == rectangle of
@@ -37,29 +40,67 @@ squaresArea({shapes, [H|T]})  ->
       false -> 0 + squaresArea({shapes, T})
    end.
 
+shapesFilter(Shape) ->
+   case Shape of
+      rectangle -> fun rectangleFilter/1;
+      triangle -> fun triangleFilter/1;
+      ellipse -> fun ellipseFilter/1
+   end.
 
+%% This function gets a shape tuple {shape, {dim, x, y}} and returns the shape
 getShape(ShapeTuple) ->
    {Shape, {_, _, _}} = ShapeTuple,
    Shape.
 
+%% This function gets a shape tuple {shape, {dim, x, y}} and returns the dimension tuple
 getShapeDim(ShapeTuple) ->
    {_, ShapeDim} = ShapeTuple,
    ShapeDim.
 
+%% Calculating the area of a rectangle if dimensions are valid.
+%% If not, returns a proper message.
 calcAreaRectangle({dim, Width, Height}) when Width > 0 , Height > 0 ->
    Width * Height;
 calcAreaRectangle({dim, _, _}) ->
    io:format("Invalid Dimensions!~n").
 
+%% Calculating the area of a triangle if dimensions are valid.
+%% If not, returns a proper message.
 calcAreaTriangle({dim, Base, Height}) when Base > 0 , Height > 0 ->
    Base * Height / 2;
 calcAreaTriangle({dim, _, _}) ->
    io:format("Invalid Dimensions!~n").
 
+%% Calculating the area of an ellipse if dimensions are valid.
+%% If not, returns a proper message.
 calcAreaEllipse({radius, Radius1, Radius2}) when Radius1 > 0 , Radius2 > 0 ->
    math:pi() * Radius1 * Radius2;
 calcAreaEllipse({radius, _, _}) ->
    io:format("Invalid Dimensions!~n").
 
+%% Calculating the area of an square if dimensions are valid.
+%% If not, returns a proper message.
 calcAreaSquare({dim, Height, Width}) when Height == Width -> math:pow(Width,2);
 calcAreaSquare({dim, _, _}) -> 0.
+
+
+rectangleFilter({shapes, []}) -> [];
+rectangleFilter({shapes, [H|T]}) ->
+   case getShape(H) == rectangle of
+      true -> [H|rectangleFilter({shapes, T})];
+      false -> rectangleFilter({shapes, T})
+   end.
+
+triangleFilter({shapes, []}) -> [];
+triangleFilter({shapes, [H|T]}) ->
+   case getShape(H) == triangle of
+      true -> [H|rectangleFilter({shapes, T})];
+      false -> rectangleFilter({shapes, T})
+   end.
+
+ellipseFilter({shapes, []}) -> [];
+ellipseFilter({shapes, [H|T]}) ->
+   case getShape(H) == ellipse of
+      true -> [H|rectangleFilter({shapes, T})];
+      false -> rectangleFilter({shapes, T})
+   end.
